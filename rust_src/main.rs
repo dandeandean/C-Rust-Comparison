8bdcaff69c37d860f7aaceb86b78fb932d8d9dcf
dandeandean <dpd5518@psu.edu>
2024-01-20 11:25:04 -0500

    messing around with rust

--- rust_src/main.rs
+++ rust_src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
+struct Student {
+    name: String,
+    ssn: String
+
+}
+impl std::fmt::Display for Student{
+    fn fmt(&self, f: & mut std::fmt::Formatter)-> std::fmt::Result {
+        write!(f,"Student( name={}, ssn={})",self.name, self.ssn)
+    }
+}
 fn main() {
-    println!("Hello World!\n");
+    println!("Enter your name: ");
+    let mut name=String::new();
+    io::stdin().read_line(&mut name).unwrap();
+    name = (*name.trim()).to_string();
+    let stud = Student {
+        name: name,
+        ssn: String::from("1234")
+    };
+
+    println!("Hello! {}",stud);
 }
