deec33874568de4885103b6411067520bde3cb69
DanDeanDean <dpd5518@psu.edu>
2024-02-20 11:45:29 -0500

    added two new printing functions for debugging

--- rust_src/src/main.rs
+++ rust_src/src/main.rs
@@ -2,5 +2,7 @@ mod maze;
 
 fn main() {
     let m = maze::build_maze(String::from("maze"));
-    dbg!("{}",m.map);
+    let _a = maze::build_agent(0,0);
+    m.pretty_print();
+    m.dbg_print();
 }
--- rust_src/src/maze.rs
+++ rust_src/src/maze.rs
@@ -13,24 +13,82 @@ use std::fs;
 // }
 
 pub struct Maze {
-    pub map: [[i32; 50]; 20] 
+    // 50x20 maze hard-coded
+    pub map: [[i32; 50]; 20],
+    pub start: (i32,i32),
+    pub fin: (i32,i32)
+}
+
+pub struct Agent{
+    pub coors: (i32,i32),
+}
+
+pub fn build_agent(x:i32, y:i32) -> Agent{
+    Agent {
+        coors: (x,y),
+    }
 }
 
 pub fn build_maze(file_name: String) -> Maze {
     let file =  fs::read_to_string(file_name).unwrap();
     let lines = file.lines();
     let mut mealhouse: [[i32; 50]; 20] = [[0;50]; 20];
+    let mut start: (i32,i32) = (0,0);
+    let mut fin: (i32,i32) = (0,0);
     for (i,line) in lines.enumerate() {
         for (j, symbol) in line.chars().enumerate() {
             match symbol {
                 ' ' => { mealhouse [i][j] = 0}
-                'S' => { mealhouse [i][j] = -100}
-                'F' => { mealhouse [i][j] = 100}
+                'S' => { 
+                    mealhouse [i][j] = -100;
+                    start = (i.try_into().unwrap(),j.try_into().unwrap());
+                }
+                'F' => { 
+                    mealhouse [i][j] = 100;
+                    fin = (i.try_into().unwrap(),j.try_into().unwrap());
+                }
                 _   => { mealhouse [i][j] = 1}
             }
 
 
         }
     }
-    Maze { map : mealhouse}
+    Maze { 
+        map : mealhouse,
+        start : start,
+        fin : fin
+    }
+}
+
+impl Maze{
+    pub fn dbg_print(&self){
+        for i in self.map {
+            for num in i {
+                print!("{}, ",num);
+            }
+            print!("\n");
+        }
+    }
+    pub fn pretty_print(&self){
+        for i in self.map {
+            for num in i {
+                match num {
+                    -100 => {
+                        print!("S");
+                    }
+                    1 => {
+                        print!("#");
+                    }
+                    100 => {
+                        print!("F");
+                    }
+                    _ => {
+                        print!(" ")
+                    }
+
+                }
+            }
+            print!("\n");
+        }
+    }
 }
